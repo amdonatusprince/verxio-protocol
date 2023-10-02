838c4a78f249900b2396020a103b4b9be18c9cb6
amdonatusprince <donatusprince@gmail.com>
2023-10-02 11:30:04 +0100

    initial commit

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+
+import { AddrMapType, MapType } from './types';
+
+export const registryAddress: AddrMapType = {
+  0: '0x0',
+  100: '0x00',
+  10200: '0x4838854e5150E4345Fb4Ae837E9FcCa40D51F3Fe'
+}
+
+export const explorer: MapType = {
+  100: 'gnosisscan.io',
+  10200: 'gnosis-chiado.blockscout.com/'
+}
